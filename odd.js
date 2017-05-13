59ea00219666a00b7e4a8bf20cd9f48ff752e017
jayl214 <jay.lin@mail.mcgill.ca>
2017-05-13 00:46:25 +0000

    add odd func

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(num){
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(5));
+console.log(isOdd(6));
+console.log(isOdd(11));
+console.log(isOdd(12));
+console.log(isOdd(123453523));
+console.log(isOdd(3456765456765678));
